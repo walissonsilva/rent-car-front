685478d71513f9c18f810f08dba3cf3d7ba6ed97
Walisson Silva <walissonsilva10@gmail.com>
2022-10-15 23:38:13 -0300

    feat: create form to search cars (UI)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Text } from '@chakra-ui/react'
+import { Box, Button, Flex, FormControl, FormLabel, Heading, Input, NumberDecrementStepper, NumberIncrementStepper, NumberInput, NumberInputField, NumberInputStepper, RangeSlider, RangeSliderFilledTrack, RangeSliderThumb, RangeSliderTrack, Select, Text } from '@chakra-ui/react'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import { Navbar } from '../components/Navbar'
@@ -12,6 +12,39 @@ const Home: NextPage = () => {
 
       <Box bgColor="gray.700" w="100vw" h="100vh">
         <Navbar />
+
+        <Box px="2rem" py="2rem">
+          <Heading color="gray.200" fontWeight="medium">Find the perfect car</Heading>
+
+          <Flex direction="column" gap="1rem" mt="2rem">
+            <FormControl mb="2">
+              <FormLabel color="white">Car name</FormLabel>
+              <Input variant='filled' _focus={{ background: "white" }} />
+            </FormControl>
+            
+            <FormControl mb="2">
+              <FormLabel color="white">Car category</FormLabel>
+              <Select variant="filled" _focus={{ background: "white" }}>
+                <option value='option1'>Option 1</option>
+                <option value='option2'>Option 2</option>
+                <option value='option3'>Option 3</option>
+              </Select>
+            </FormControl>
+
+            <FormControl mb="2">
+              <FormLabel color="white">Max rent value</FormLabel>
+              <NumberInput placeholder="Max value" step={50} defaultValue={150} min={50} max={1000} variant="filled" _focus={{ background: "white" }}>
+                <NumberInputField _focus={{ background: "white" }} />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
+            </FormControl>
+
+            <Button colorScheme="purple" mt="2">Search</Button>
+          </Flex>
+        </Box>
       </Box>
     </>
   )
